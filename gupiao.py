4a51f0ed6f17a496c2e8038dbd90d49cdbeecea0
wawatu <77338378@qq.com>
2017-08-21 19:14:42 +0800

    Update gupiao.py

--- gupiao.py
+++ gupiao.py
@@ -4,27 +4,66 @@ from bs4 import BeautifulSoup
 import traceback
 
 #获取url网页
-def getHTMLText(url):
-	return""
+def getHTMLText(url, code='utf-8'):
+	try:
+		r.requests.get(url, timeout = 30)
+		r.raise_for_status()
+		r.encoding = code
+		return r.text
+	except:
+		return""
 
 #获取股票信息列表	
 def getStockList(lst, stockURL): #列表类型存储股票的信息，股票URL网站
-	return""
+	html = getHTMLText(stockUrl, 'GB2312')
+	soup = BeautifulSoup(html, 'html.parser')
+	a = soup.find_all('a')
+	for i in a:
+		try:
+			href = i.attrs['href']
+			lst.append(re.findall(r"[s][hz]\d{6}", href)[0])
+		except:
+			continue
 	
 #获取每只个股信息
 def getStockInfo(lst, stockURL, fpath):
-	return""
-	
-	
+    count = 0
+	for stock in lst:
+		url = stockUrl + stock + '.html'
+		html = getHTMLText(url)
+		try:
+			if html == ""
+				continue
+			infoDict = ()
+			soup = BeautifulSoup(html, 'html.parser')
+			stockInfo = soup.find('div', attrs={'class:stock-bets'})
+			
+			name = stockInfo.find_all(attrs={'class':'bets-name'})[0]
+			infoDict.update({'股票名称': name.text.split()[0]})
+			
+			keyList = stockInfo.find_all('dt')
+			valueList = stockInfo.find_all('dd')
+			for i in range(len(keyList)):
+				key = keyList[i].text
+				val = valueList[i].text
+				infoDict[key] = val
+				
+			with open(fpath, 'a', encoding='utf-8') as f
+				f.write(str(infoDict) + '\n')
+				count = count + 1
+				print("\r当前进度:{:.2f}%".format(count*100/len(lst)), end="")
+		except:
+			count = count + 1
+			print("\r当前进度:{:.2f}%".format(count*100/len(lst)), end="")
+			continue
 def main():
-	stock_list_url = 'http://quote.eastmoney.com/stocklist'
+	stock_list_url = 'http://quote.eastmoney.com/stocklist.html'
 	stock_info_url = 'https://gupiao.baidu.com/stock'
 	output_file = 'D://BaiduStockInfo.txt' #保存信息路径
 	slist = []
 	getStockList(slist, stock_list_url)
-	getStockInfo(slist, stock_info_url)
+	getStockInfo(slist, stock_info_url, output_file)
 	
-	
-	
-	
-	
+main()
+
+
